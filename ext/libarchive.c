e057dd1948ddcc1eef9e04ba4a0d000280044f49
winebarrel <sgwr_dts@yahoo.co.jp>
2010-12-12 03:34:18 +0900

    change export name

--- ext/libarchive.c
+++ ext/libarchive.c
@@ -21,7 +21,7 @@ VALUE rb_libarchive_s_version_string(VALUE self) {
 #endif
 }
 
-void DLLEXPORT Init_libarchive_ruby() {
+void DLLEXPORT Init_libarchive() {
   rb_mArchive = rb_define_module("Archive");
   rb_define_const(rb_mArchive, "VERSION", rb_str_new2(VERSION));
 
